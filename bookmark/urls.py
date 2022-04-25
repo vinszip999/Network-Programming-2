a3d6ccbdb9267bd1091688ce0a00e1d195b3709f
vinszip999 <w2011@e-mirim.hs.kr>
2022-04-25 14:45:07 +0900

    urls bookmark:delete

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from bookmark.views import BookmarkListView, BookmarkCreateView, BookmarkDetailView, BookmarkUpdateView
+from bookmark.views import BookmarkListView, BookmarkCreateView, BookmarkDetailView, BookmarkUpdateView, \
+    BookmarkDeleteView
 
 app_name = 'bookmark'
 
@@ -9,4 +10,6 @@ urlpatterns = [
     path('add/', BookmarkCreateView.as_view(), name='add'),  # bookmark:add
     path('detail/<int:pk>/', BookmarkDetailView.as_view(), name='detail'),  # bookmark:detail
     path('edit/<int:pk>/', BookmarkUpdateView.as_view(), name='edit'),  # bookmark:edit
+    # 특정 북마크를 삭제할 것이기 때문에 그것을 가리킬 pk가 있어야 한다. 그러므로, delete도 pk가 있어야 한다.
+    path('delete/<int:pk>/', BookmarkDeleteView.as_view(), name='delete'),  # bookmark:delete
 ]
